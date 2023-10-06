0c8d49f55ad7ea61dcc79b32c630c77df157d32a
Swarnim Chandve <swarnimchandve23@gmail.com>
2023-10-06 13:11:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let toggle = document.querySelector('.toggle');
+let menu = document.querySelector('.menu');
+
+
+
+toggle.addEventListener('click',function(){
+    menu.classList.toggle('active');
+})
